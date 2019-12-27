051d50b427e5055c338364f3bbc0c00cea2fb6d3
Ralph Ridley <rkridley@outlook.com>
2019-12-27 14:36:05 +0000

    Compatibility tweak

--- Vulkan-Pipeline-Assistant/Shaders/compile.bat
+++ Vulkan-Pipeline-Assistant/Shaders/compile.bat
@@ -1,3 +1,3 @@
-C:\VulkanSDK\1.1.126.0\Bin\glslc.exe vs_test.vert -c -o ../../build-VulkanPipelineAssistant-Desktop_x86_windows_msvc2017_pe_64bit-Debug/debug/vs_test.spv
-C:\VulkanSDK\1.1.126.0\Bin\glslc.exe fs_test.frag -c -o ../../build-VulkanPipelineAssistant-Desktop_x86_windows_msvc2017_pe_64bit-Debug/debug/fs_test.spv
+%VK_SDK_PATH%\Bin\glslc.exe vs_test.vert -c -o ../Build/shaders/vs_test.spv
+%VK_SDK_PATH%\Bin\glslc.exe fs_test.frag -c -o ../build/shaders/fs_test.spv
 pause
--- Vulkan-Pipeline-Assistant/vulkanrenderer.cpp
+++ Vulkan-Pipeline-Assistant/vulkanrenderer.cpp
@@ -255,7 +255,7 @@ void VulkanRenderer::CreateRenderPass(PipelineConfig& config) {
 
 void VulkanRenderer::CreatePipeline(PipelineConfig& config) {
     QVector<VkPipelineShaderStageCreateInfo> shaderStageInfos;
-    m_shaderAnalytics->LoadShaders("/vs_test.spv", "/fs_test.spv");
+    m_shaderAnalytics->LoadShaders("/../shaders/vs_test.spv", "/../shaders/fs_test.spv");
     VkPipelineShaderStageCreateInfo shaderCreateInfo;
     if (m_shaderAnalytics->GetStageCreateInfo(ShaderStage::VETREX, shaderCreateInfo)) shaderStageInfos.push_back(shaderCreateInfo);
     if (m_shaderAnalytics->GetStageCreateInfo(ShaderStage::FRAGMENT, shaderCreateInfo)) shaderStageInfos.push_back(shaderCreateInfo);
